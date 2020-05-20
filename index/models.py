87948b3ee5df877ac1200f1c31b92cb3e10f6994
Elon <elon@Elons-iMac.local>
2020-05-20 12:37:59 -0700

    initial

---
+++ index/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Painting(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+    translation = models.CharField(max_length=100, null=True, blank=True)
+    viewed = models.BooleanField(default=False)
+    painting = models.ImageField(upload_to='images/', null=True)
+
+    def __str__(self):
+        return str(self.name)
---
+++ index/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+def index(request):
+
+    obj = {}
+
+    return render(request, 'index.html', obj)
