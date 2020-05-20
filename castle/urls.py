6e963be6181c9178fa86934418d2ab595e397756
Elon <elon@Elons-iMac.local>
2020-05-20 12:37:59 -0700

    initial

---
+++ castle/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    path('admin/', admin.site.urls),
+    path('', include('index.urls')),
+]
