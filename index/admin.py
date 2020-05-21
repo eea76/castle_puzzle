b1e2817b38142c4e25474a04b039b732b5c4391c
Elon <elon@Elons-iMac.local>
2020-05-21 11:45:21 -0700

    user accounts working

--- index/admin.py
+++ index/admin.py
@@ -3,3 +3,5 @@ from django.contrib import admin
 from index.models import *
 
 admin.site.register(Painting)
+admin.site.register(UserPainting)
+admin.site.register(Attempt)
--- index/models.py
+++ index/models.py
@@ -1,15 +1,29 @@
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 
 class Painting(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
     translation = models.CharField(max_length=100, null=True, blank=True)
-    viewed = models.BooleanField(default=False)
     painting = models.ImageField(upload_to='images/', null=True)
 
     def __str__(self):
         return str(self.name)
 
 
+class UserPainting(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    painting = models.ForeignKey(Painting, null=True, on_delete=models.CASCADE)
+    viewed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.user).title() + ' viewed ' + str(self.painting).upper()
+
+
+class Attempt(models.Model):
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    guess = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user).title() + ' tried: ' + str(self.guess).upper()
