f7186c2960f05748574770af4a2f2b81b55561fb
Elon <elon@Elons-iMac.local>
2020-05-24 09:24:08 -0700

    delete unnecessary db fields and combined unnamed and painting views into one

--- index/urls.py
+++ index/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('about', views.about, name="about"),
     path('signup', views.signup, name="signup"),
     path('painting/<str:painting_name>/', views.painting, name='painting'),
-    path('unnamed', views.unnamed, name='unnamed'),
     path('view_painting', views.view_painting, name='view_painting'),
     path('painting_guess', views.painting_guess, name='painting_guess'),
     path('reset', views.reset, name='reset'),
