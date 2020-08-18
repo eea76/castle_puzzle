b30780ea098eb91a7570cd7380b9f89d086e865e
Elon <elon@Elons-iMac.local>
2020-08-18 12:34:54 -0700

    delete adventure

--- adventure/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from adventure.models import *
-
-
-admin.site.register(Door)
-admin.site.register(Option)
-admin.site.register(Chamber)
-admin.site.register(PreviousOption)
